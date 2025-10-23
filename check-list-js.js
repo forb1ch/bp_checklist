48392955cb643552ad0eea1de0370b6d07724bba
forb1ch <illya.ostrovsky.4b@gmail.com>
2025-10-23 19:18:12 +0300

    Updated functionality for New quest button.

--- check-list-js.js
+++ check-list-js.js
@@ -12,207 +12,247 @@
             rewardPrice: 4,
             title: '3 часа в онлайне.',
             completedCount: 0,
-            completed: false,
+            deleted: false,
           },
           lottery: {
             rewardType: 'lottery',
             rewardPrice: 2,
             title: 'Купить лотерейный билет',
+            deleted: false,
           },
           cinema: {
             rewardType: 'cinema',
             rewardPrice: 2,
             title: 'Добавить 5 видео в кинотеатре',
+            deleted: false,
           },
           film_studio: {
             rewardType: 'film_studio',
             rewardPrice: 4,
             title: 'Арендовать киностудию',
+            deleted: false,
           },
           ammunation: {
             rewardType: 'ammunation',
             rewardPrice: 2,
             title: 'Успешная тренировка в тире',
+            deleted: false,
           },
           site: {
             rewardType: 'site',
             rewardPrice: 2,
             title: 'Зайти на любой сайт',
+            deleted: false,
           },
           match: {
             rewardType: 'match',
             rewardPrice: 2,
             title: 'Поставить любую реаку в match.',
+            deleted: false,
           },
           basketball: {
             rewardType: 'basketball',
             rewardPrice: 2,
             title: 'Забить два гола в баскете.',
+            deleted: false,
           },
           football: {
             rewardType: 'football',
             rewardPrice: 2,
             title: 'Забить два гола в футболе.',
+            deleted: false,
           },
           tennis: {
             rewardType: 'tennis',
             rewardPrice: 2,
             title: 'Поиграть 1 минуту в настольный теннис.',
+            deleted: false,
           },
           bigtennis: {
             rewardType: 'bigtennis',
             rewardPrice: 2,
             title: 'Поиграть 1 минуту в большой теннис.',
+            deleted: false,
           },
           voleyball: {
             rewardType: 'voleyball',
             rewardPrice: 2,
             title: 'Забить 10 голов в воллейболе.',
+            deleted: false,
           },
           darts: {
             rewardType: 'darts',
             rewardPrice: 2,
             title: 'Победа в дартс.',
+            deleted: false,
           },
           arms: {
             rewardType: 'arms',
             rewardPrice: 2,
             title: 'Победа в арм ресслинге.',
+            deleted: false,
           },
           service: {
             rewardType: 'service',
             rewardPrice: 2,
             title: 'Починить деталь в автосервисе.',
+            deleted: false,
           },
           bigcasino: {
             rewardType: 'bigcasino',
             rewardPrice: 6,
             title: 'Ставка в колесе удачи в казино (межсерверное колесо).',
+            deleted: false,
           },
           brawl: {
             rewardType: 'brawl',
             rewardPrice: 2,
             title: 'Зайти в любой канал в Brawl.',
+            deleted: false,
           },
           petball: {
             rewardType: 'petball',
             rewardPrice: 4,
             title: 'Кинуть мяч питомцу 15 раз.',
+            deleted: false,
           },
           build: {
             rewardType: 'build',
             rewardPrice: 4,
             title: '25 действий на стройке',
+            deleted: false,
           },
           mine: {
             rewardType: 'mine',
             rewardPrice: 4,
             title: '25 действий в шахте',
+            deleted: false,
           },
           port: {
             rewardType: 'port',
             rewardPrice: 4,
             title: '25 действий в порту',
+            deleted: false,
           },
           carting: {
             rewardType: 'carting',
             rewardPrice: 2,
             title: 'Выиграть гонку в картинге',
+            deleted: false,
           },
           ferm: {
             rewardType: 'ferm',
             rewardPrice: 2,
             title: '10 действий на ферме',
+            deleted: false,
           },
           fire_work: {
             rewardType: 'fire_work',
             rewardPrice: 2,
             title: 'Потушить 25 огоньков пожарным',
+            deleted: false,
           },
           race: {
             rewardType: 'race',
             rewardPrice: 2,
             title: 'Проехать 1 уличную гонку',
+            deleted: false,
           },
           treasure: {
             rewardType: 'treasure',
             rewardPrice: 2,
             title: 'Выкопать 1 сокровище(не мусор)',
+            deleted: false,
           },
           hunting: {
             rewardType: 'hunting',
             rewardPrice: 4,
             title: '5 раз снять 100% шкуру с животных',
+            deleted: false,
           },
           contraband: {
             rewardType: 'contraband',
             rewardPrice: 4,
             title: 'Сдать 5 контрабанды',
+            deleted: false,
           },
           tk: {
             rewardType: 'tk',
             rewardPrice: 2,
             title: 'Выиграть 5 игр в тренировочном комплексе со ставкой',
+            deleted: false,
           },
           maze: {
             rewardType: 'maze',
             rewardPrice: 2,
             title: 'Выиграть 3 любых игры на арене со ставкой',
+            deleted: false,
           },
           casino: {
             rewardType: 'casino',
             rewardPrice: 4,
             title: 'Нули в казино',
+            deleted: false,
           },
           post: {
             rewardType: 'post',
             rewardPrice: 2,
             title: '10 посылок на почте',
+            deleted: false,
           },
           bus: {
             rewardType: 'bus',
             rewardPrice: 4,
             title: '2 круга на любом маршруте автобусника',
+            deleted: false,
           },
           gym: {
             rewardType: 'gym',
             rewardPrice: 2,
             title: '20 подходов в зале',
+            deleted: false,
           },
           fish: {
             rewardType: 'fish',
             rewardPrice: 8,
             title: 'Поймать 20 рыб.',
+            deleted: false,
           },
           metro: {
             rewardType: 'metro',
             rewardPrice: 4,
             title: 'Проехать 1 станцию на метро.',
+            deleted: false,
           },
           pet: {
             rewardType: 'pet',
             rewardPrice: 4,
             title: '15 выполненных питомцем команд.',
+            deleted: false,
           },
           club: {
             rewardType: 'club',
             rewardPrice: 8,
             title: 'Выполнить 2 квеста любых клубов 4/8 BP.',
+            deleted: false,
           },
           mafia: {
             rewardType: 'mafia',
             rewardPrice: 6,
             title: 'Сыграть в мафию в казино.',
+            deleted: false,
           },
           trucker: {
             rewardType: 'trucker',
             rewardPrice: 4,
             title: 'Выполнить 3 заказа дальнобойщиком.',
+            deleted: false,
           },
           airdrop: {
             rewardType: 'airdrop',
             rewardPrice: 8,
             title: 'Принять участие в двух аирдропах.',
+            deleted: false,
           },
         },
         tasks: [
@@ -223,9 +263,11 @@
       };
 
       // Modal functions
-      function openModal(modalId) {
+      function openModal() {
+        const modalId = state.tasksToAdd.length > 0 ? 'newTaskModal' : 'noTaskModal' ;
         const modal = document.getElementById(modalId);
         modal.classList.add("active");
+
         // Prevent body scrolling when modal is open
         document.body.style.overflow = "hidden";
       }
@@ -271,13 +313,17 @@
         updateAddTaskList();
       }
 
-      
       // Update add task list
       function updateAddTaskList() {
-        const taskListSelect = document.getElementById("taskRewardType");
-        taskListSelect.innerHTML = "";
-        
-        state.tasks.forEach((task, index) => {
+        const taskListSelectWrapper = document.getElementById("taskRewardType");
+        taskListSelectWrapper.innerHTML = "";
+
+        const taskListSelect = document.createElement("select");
+        taskListSelect.id = 'taskRewardTypeSelect';
+        taskListSelect.className = 'w-full bg-zinc-700 rounded-lg p-2 text-white border border-gray-600 focus:border-purple-500 focus:ring-2 focus:ring-purple-500 focus:outline-none transition-all duration-300';
+        taskListSelectWrapper.appendChild(taskListSelect);
+
+        state.tasksToAdd.forEach((task, index) => {
           let price = state.vip == true ? task.rewardPrice : task.rewardPrice / 2;
           price = state.x2 == true ? price * 2 : price;
           const taskElement = document.createElement("option");
@@ -352,14 +398,22 @@
       // Task management functions
       function addNewTask(event) {
         event.preventDefault();
-        const rewardType = document.getElementById("taskRewardType").value;
+        const rewardType = document.getElementById("taskRewardTypeSelect").value;
         const rewardPrice = state.taskReward[rewardType].rewardPrice;
         const title = state.taskReward[rewardType].title;
+        let deletedTaskIndex = 0;
+        const tasksListToAdd = state.tasksToAdd;
+        let taskListRemoved = [];
+
+
+        taskListRemoved = tasksListToAdd.filter(item => item.rewardType !== rewardType);
+        state.tasksToAdd = taskListRemoved;
 
         state.tasks.push({
           title,
           rewardPrice,
           rewardType,
+          deleted: false,
           completed: false,
           completedCount: 0,
         });
@@ -487,6 +541,22 @@
       }
 
       function deleteTask(index) {
+        const deletedTask = state.tasks[index];
+        const rewardType = state.tasks[index].rewardType;
+        const rewardPrice = state.tasks[index].rewardPrice;
+        const title = state.tasks[index].title;
+
+        let deletedTaskObj = {
+          title,
+          rewardPrice,
+          rewardType,
+          deleted: true,
+          completed: false,
+          completedCount: 0,
+        };
+
+        state.tasksToAdd.push(deletedTaskObj);
+
         // Add delete animation
         const taskElement = document.querySelector(
           `#taskList > div:nth-child(${index + 1})`,
@@ -580,7 +650,7 @@
                 var obj = tasksList[tasks];
                 obj = {...obj, completed};
 
-                tasksListDefault.push(obj);
+                if (!obj.deleted) tasksListDefault.push(obj);
             }
 
             let coinsLeft = 0;
